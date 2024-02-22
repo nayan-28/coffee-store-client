1570cf61639edc511d8225d212ea27bd8fcd0422
nayan-28 <nayanmalakar720@gmail.com>
2024-02-22 23:56:47 +0600

    user authentication completed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD_O5fTxUeA1ZhaNPauC4isSot5raKrBd0",
+    authDomain: "coffee-store-8e84c.firebaseapp.com",
+    projectId: "coffee-store-8e84c",
+    storageBucket: "coffee-store-8e84c.appspot.com",
+    messagingSenderId: "390397235107",
+    appId: "1:390397235107:web:1fdb412eded61f119b5416"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
